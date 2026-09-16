8ddb7f396c37ee787fb49e6ae4560b6e7790029c
niteshgopal03 <niteshgopal92@gmail.com>
2026-09-16 23:31:41 +0530

    Added Admin Dashboard APIs

--- backend/main.py
+++ backend/main.py
@@ -1,30 +1,34 @@
 from fastapi import FastAPI
-from routes.employee import router as employee_router
+
 from routes.auth import router as auth_router
+from routes.employee import router as employee_router
+from routes.hr import router as hr_router
 from routes.attendance import router as attendance_router
+from routes.leave import router as leave_router
+from routes.password import router as password_router
+from routes.performance import router as performance_router
+from routes.ai import router as ai_router
+from routes.admin_dashboard import router as admin_dashboard_router
+
 
 app = FastAPI(
-    title="Smart Employee Intelligence System API",
-    description="Backend API for Smart Employee Intelligence System",
+    title="Employee Management System API",
     version="1.0.0"
 )
 
+app.include_router(auth_router)
+app.include_router(hr_router)
+app.include_router(employee_router)
+app.include_router(attendance_router)
+app.include_router(leave_router)
+app.include_router(password_router)
+app.include_router(performance_router)
+app.include_router(ai_router)
+app.include_router(admin_dashboard_router)
 
 @app.get("/")
 def home():
+
     return {
-        "message": "Welcome to Smart Employee Intelligence System API",
-        "status": "Running"
+        "message": "Employee Management System API Running Successfully"
     }
-
-
-@app.get("/health")
-def health_check():
-    return {
-        "status": "Healthy"
-    }
-
-
-app.include_router(employee_router)
-app.include_router(auth_router)
-app.include_router(attendance_router)
